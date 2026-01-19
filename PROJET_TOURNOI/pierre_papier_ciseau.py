ba812f010835e02fcb9b8517dae835a1353fe95c
Dylane KAMGA <dylanekamga100@gmail.com>
2026-01-19 13:46:43 +0100

    tous mon travail

--- PROJET_TOURNOI/pierre_papier_ciseau.py
+++ PROJET_TOURNOI/pierre_papier_ciseau.py
@@ -1,7 +1,35 @@
+import random
 def jouer_pierre_papier_ciseau():
-    print("Bienvenue dans Pierre - Papier - Ciseaux")
 
-    # ton code existant ici
-    # input(), logique du jeu, etc.
+    manches = int(input("Combien de manches voulez-vous jouer ? "))
 
-    print("Fin du jeu Pierre - Papier - Ciseaux")
+    score_joueur = 0
+    score_ordi = 0
+
+    options = ("P", "F", "C")
+
+    while score_joueur < manches and score_ordi < manches:
+        choix_joueur = input("Que jouez-vous ? [P]ierre, [F]euille, [C]iseaux ").upper()
+
+        while choix_joueur not in options:
+            choix_joueur = input("Choix valides : P F ou C ").upper()
+
+        choix_ordi = random.choice(options)
+
+        print(choix_joueur, "x", choix_ordi)
+
+        if choix_joueur == choix_ordi:
+            print("Égalité")
+        elif options.index(choix_joueur) == (options.index(choix_ordi) + 1) % 3:
+            score_joueur += 1
+            print("Vous remportez la manche 👨‍💻")
+        else:
+            score_ordi += 1
+            print("L'ordinateur remporte la manche 🤖")
+
+        print("[", score_joueur, "-", score_ordi, "]")
+
+    if score_joueur == manches:
+        print("Vous avez gagné la partie ✅")
+    else:
+        print("L'ordinateur gagne la partie ❌")
